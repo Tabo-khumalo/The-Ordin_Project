de2a076effbf68d6a5cb53858bd9887bddb2100a
Tabo-khumalo <tabo.khumalo26@gmail.com>
2024-11-24 15:47:27 +0200

    Finished code and README file
    finalized the code for the game and edit the README.md file.

--- Assigments/rock-paper_Scissors/js/script.js
+++ Assigments/rock-paper_Scissors/js/script.js
@@ -1,132 +1,138 @@
 
-let playerWin = 0;
-let playerLose = 0;
-let computerWin = 0;
-let computerLose = 0;
-
-let human = getHumanChoice();
-let comp = getComputerchoice();
-
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerchoice();
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerchoice() {
     let computerMove = Math.floor(Math.random() * 75);
 
-    let computerPlay = "";
-
     if (computerMove >= 0 && computerMove <= 25) {
-        computerPlay  = "Rock";
+        return "Rock";
     } 
     else if(computerMove > 25 && computerMove <= 50) {
-        computerPlay  = "Paper";
+        return "Paper";
     } 
      else {
-        computerPlay  = "Scissors";
+        return "Scissors";
      }
-    
-    return computerPlay 
-}
+    }
 
 
 function getHumanChoice(){
     let userPrompt = prompt("Enter Rock, Paper or Scissors to Play", "");
-    let humanPlay = "";
     let userPromptFirstLetter = userPrompt.at(0).toLocaleUpperCase();
     let userPromptLowerCase = userPrompt.slice(1).toLocaleLowerCase()
 
-    humanPlay = userPromptFirstLetter + userPromptLowerCase;
-
-    return humanPlay;
-    
+    return userPromptFirstLetter + userPromptLowerCase;  
 }
+
 
 function playRound(humanChoice, computerChoice) {
 
+    let result = "";
+
     if(humanChoice === "Rock" && computerChoice === "Scissors") {
-        playerWin++
-        computerLose++
-        console.log(`
+        humanScore++;
+        result = console.log(`
 
             You picked ${humanChoice} and Computer picked ${computerChoice} you win!!
             Scores: 
-            Player wins: ${playerWin}
-            Computer Wins: ${computerWin}
-            Player Loses: ${playerLose} 
-            Computer Loses ${computerLose}
+            Player Score: ${humanScore}
+            Computer Score: ${computerScore}
+            
         `)
     } 
     else if (humanChoice === "Scissors" && computerChoice === "Paper") { 
-        playerWin++
-        computerLose++
-        console.log(`
+        humanScore++;
+        result = console.log(`
 
             You picked ${humanChoice} and Computer picked ${computerChoice} you win!!
             Scores: 
-            Player wins: ${playerWin}
-            Computer Wins: ${computerWin}
-            Player Loses: ${playerLose} 
-            Computer Loses ${computerLose}
+            Player Score: ${humanScore}
+            Computer Score: ${computerScore}
         `)
     }
     else if (humanChoice === "Paper" && computerChoice === "Rock") { 
-        playerWin++
-        computerLose++
-        console.log(`
+        humanScore++;
+    
+        result = console.log(`
 
             You picked ${humanChoice} and Computer picked ${computerChoice} you win!!
             Scores: 
-            Player wins: ${playerWin}
-            Computer Wins: ${computerWin}
-            Player Loses: ${playerLose} 
-            Computer Loses ${computerLose}
+            Player Score: ${humanScore}
+            Computer Score: ${computerScore}
         `)
     }
     else if (humanChoice === "Scissors" && computerChoice === "Rock") { 
-        playerLose++
-        computerWin++
-        console.log(`
+        ccomputerScore++;
+        result = console.log(`
 
             You picked ${humanChoice} and Computer picked ${computerChoice} Computer wins!!
             Scores: 
-            Player wins: ${playerWin}
-            Computer Wins: ${computerWin}
-            Player Loses: ${playerLose} 
-            Computer Loses ${computerLose}
+            Player Score: ${humanScore}
+            Computer Score: ${computerScore}
         `)
     }
     else if (humanChoice === "Rock" && computerChoice === "Paper") { 
-        playerLose++
-        computerWin++
-        console.log(`
+        computerScore++;
+        result = console.log(`
 
             You picked ${humanChoice} and Computer picked ${computerChoice} Computer wins!!
             Scores: 
-            Player wins: ${playerWin}
-            Computer Wins: ${computerWin}
-            Player Loses: ${playerLose} 
-            Computer Loses ${computerLose}
+            Player Score: ${humanScore}
+            Computer Score: ${computerScore}
         `)
     }
     else if (humanChoice === "Paper" && computerChoice === "Scissor") { 
-        playerLose++
-        computerWin++
-        console.log(`
+        computerScore++;
+        result = console.log(`
 
             You picked ${humanChoice} and Computer picked ${computerChoice} Computer wins!!
             Scores: 
-            Player wins: ${playerWin}
-            Computer Wins: ${computerWin}
-            Player Loses: ${playerLose} 
-            Computer Loses ${computerLose}
+            Player Score: ${humanScore}
+            Computer Score: ${computerScore}
         `)
     } else  if(humanChoice === computerChoice){
-        console.log(`You picked ${humanChoice} and Computer picked ${computerChoice} it's a tie`)
+        result = console.log(`You picked ${humanChoice} and Computer picked ${computerChoice} it's a tie`)
     }
 
+    return result
 }
 
-console.log(human)
-console.log(comp)
-playRound(human, comp)
+
+
+
+function playGame() {
+    for(let i = 1; i < 6; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerchoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    if(computerScore > humanScore) {
+        return `
+        Computer Score: ${computerScore} - Human Score: ${humanScore}
+        the computer beat you. try again`
+    }
+    else if(computerScore < humanScore) {
+        return `
+        Computer Score: ${humanScore} - Human Score: ${computerScore}
+        You beat the computer, way to go!`
+    } else {
+        return `
+        Computer Score: ${humanScore} - Human Score: ${computerScore}
+        it's a tie, not bad!`
+    }
+}
+
+console.log(playGame())
+
+
+
+
+
+
 
 
 
